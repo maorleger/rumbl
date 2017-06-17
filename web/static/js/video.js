54ea15c2fb5225c9645e00b14a2d58f548ddcb1f
Maor Leger <maor.leger@gmail.com>
2017-06-17 12:34:32 -0600

    Handling disconnects, finishes chapter 10

--- web/static/js/video.js
+++ web/static/js/video.js
@@ -44,12 +44,25 @@ let Video = {
     })
 
     vidChannel.on("new_annotation", (resp) => {
+      vidChannel.params.last_seen_id = resp.id
       this.renderAnnotation(msgContainer, resp)
     })
 
+    msgContainer.addEventListener("click", e => {
+      e.preventDefault()
+      let seconds = e.target.getAttribute("data-seek") || e.target.parentNode.getAttribute("data-seek")
+      if (!seconds) { return }
+
+      Player.seekTo(seconds)
+    })
+
     vidChannel.join()
-      .receive("ok", ({annotations}) => {
-        annotations.forEach(ann => this.renderAnnotation(msgContainer, ann))
+      .receive("ok", resp => {
+        let ids = resp.annotations.map(ann => ann.id)
+        if (ids.length > 0) {
+          vidChannel.params.last_seen_id = Math.max(...ids) 
+        }
+        resp.annotations.forEach(ann => this.renderAnnotation(msgContainer, ann))
       })
       .receive("error", reason => console.log("join failed", reason))
   }
